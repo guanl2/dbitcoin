3bce46fa616a646715e5e96743d6de0b998dac5e
gl777 <saiwin7@163.com>
2020-06-22 16:07:44 +0800

    init

---
+++ example/dbitcoin_example.dart
@@ -0,0 +1,6 @@
+import 'package:dbitcoin/dbitcoin.dart';
+
+main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/dbitcoin.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dbitcoin;
+
+export 'src/dbitcoin_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
